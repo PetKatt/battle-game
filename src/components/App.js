ec80517ccf950bd38aa0ef8acc9b82b5fc95d3db
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-24 09:36:43 +0100

    react-router DONE

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {
 	BrowserRouter as Router,
-	Route
+	Route,
+	Switch
 } from "react-router-dom";
 
 import Popular from "./Popular";
 import Nav from "./Nav";
+import Home from "./Home";
+import Battle from "./Battle";
 
 class App extends React.Component {
   render() {
@@ -13,7 +16,14 @@ class App extends React.Component {
     	<Router>
 	      <div className="container">
 	      	<Nav />
-	        <Route path="/popular" component={Popular} />
+	      	<Switch>
+		      	<Route exact path="/" component={Home} />
+		      	<Route exact path="/battle" component={Battle} />
+		        <Route path="/popular" component={Popular} />
+		        <Route render={() => (
+		        	<p>Not Found ;/</p>
+		        )} />
+		      </Switch>
 	      </div>
 	    </Router>
     );
